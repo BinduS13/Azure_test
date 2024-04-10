855102db4fd7b4e62a75594a32b717aafda851c6
Bindu Madhavi Siraparapu <2837072@vikes.csuohio.edu>
2024-04-10 10:51:57 -0500

    Initial commit

---
+++ Hw_App/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Hw_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Hw_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Content("Hello, World!");
+            //return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
